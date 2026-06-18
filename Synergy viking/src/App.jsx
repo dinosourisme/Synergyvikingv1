cc05eac173a3a6454ae34b167bcd497df3c8e441
Taha-Sayyed <tahasayyedk00@gmail.com>
2026-06-18 16:18:05 +0530

    Complete about-us Page

--- Synergy viking/src/App.jsx
+++ Synergy viking/src/App.jsx
@@ -3,6 +3,7 @@ import './index.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
 import Services from './pages/Services/Services';
+import AboutUsPage from "./pages/aboutus/main.jsx"
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<div className="p-10 text-white">Home Page</div>} />
         <Route path="/services" element={<Services />} />        <Route path="/products" element={<div className="p-10 text-white">Products Page</div>} />
-        <Route path="/about" element={<div className="p-10 text-white">About Page</div>} />
+        <Route path="/about" element={<AboutUsPage/>} />
         <Route path="/contact" element={<div className="p-10 text-white">Contact Page</div>} />
       </Routes>
       <Footer />
